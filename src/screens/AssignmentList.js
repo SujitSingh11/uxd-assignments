dece60371c5b6bd8764a36c317e4e0698315bce7
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-08-26 21:28:03 +0530

    Update AssignmentList.js

--- src/screens/AssignmentList.js
+++ src/screens/AssignmentList.js
@@ -1,10 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Material UI
+import { Paper, Typography, CardActionArea } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    textAlign: "-webkit-center",
+    marginTop: "20vh",
+  },
+  paper: {
+    width: "500px",
+    padding: "20px",
+  },
+  title: {
+    letterSpacing: "2px",
+    fontWeight: "500",
+    fontFamily: "'Quicksand', sans-serif",
+  },
+  links: {
+    textDecoration: "none",
+    color: "black",
+    fontWeight: "400",
+    fontFamily: "'Quicksand', sans-serif",
+    fontSize: "25px",
+  },
+  linksDiv: {
+    marginTop: "10px",
+  },
+  paperLinks: {
+    padding: "10px 0px",
+    backgroundColor: "#4caf50",
+  },
+}));
+
 const AssignmentList = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Link to="/assignment1">Assignment 1</Link>
+    <div className={classes.root}>
+      <Paper elevation={3} className={classes.paper}>
+        <div>
+          <Typography variant="h4" component="h4" className={classes.title}>
+            Assignment List
+          </Typography>
+        </div>
+        <div className={classes.linksDiv}>
+          <CardActionArea component={Link} to="/assignment1">
+            <Paper elevation={3} className={classes.paperLinks}>
+              <Typography
+                variant="p"
+                component="span"
+                className={classes.links}
+              >
+                Assignment 2 - Login Design
+              </Typography>
+            </Paper>
+          </CardActionArea>
+        </div>
+      </Paper>
     </div>
   );
 };
